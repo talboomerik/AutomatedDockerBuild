dbd23314074561c80c939b725e6519ab95e37a00
Erik Talboom <talboomerik@gmail.com>
2016-11-24 09:26:03 +0000

    Removed Erik from existence

--- JavaHelloWorld.java
+++ JavaHelloWorld.java
@@ -1,7 +1,6 @@
 public class JavaHelloWorld							{
 	public static void main (String [ ] args)				{
 		System.out.println("Java hello world")				;
-		System.out.println("Erik is awesome")				;
 		System.out.println("He really is")				;
 		System.out.println("No seriously, he's really cool")		;
 										}
